463eefb51c41dab7dea1318978674cc56849d040
Antonio Salinas García (vedoque.com) <73043287+antoniocesur@users.noreply.github.com>
2023-11-18 08:32:59 +0100

    cambios 05-11

--- src/main/java/com/example/reto/RetoApplication.java
+++ src/main/java/com/example/reto/RetoApplication.java
@@ -6,10 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class RetoApplication {
 
 	public static void main(String[] args) {
+		String command = "C:\\xampp\\mysql\\bin\\mysqld.exe";
+		try{
+			Process process = Runtime.getRuntime().exec(command);
+			Process process1 = Runtime.getRuntime().exec("C:\\xampp\\apache\\bin\\httpd.exe");
+		}catch (IOException e){
+			e.printStackTrace();
+		}
 		SpringApplication.run(RetoApplication.class, args);
 	}
 	@Bean
